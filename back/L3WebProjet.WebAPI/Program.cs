43372833ab33165e35986ca19fa0d00f91e3e9ec
buathier.tom <buathier_tom@orange.fr>
2025-06-04 14:17:53 +0200

    fix: Import services in progam.cs (#16)

--- back/L3WebProjet.WebAPI/Program.cs
+++ back/L3WebProjet.WebAPI/Program.cs
@@ -2,6 +2,9 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 using Microsoft.EntityFrameworkCore;
 using L3WebProjet.DataAccess;
 using L3WebProjet.DataAccess.Interfaces;
+using L3WebProjet.Business.Implementations;
+using L3WebProjet.Business.Interfaces;
+using L3WebProjet.DataAccess.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IStoreService, StoreService>();
+builder.Services.AddScoped<ISectionService, SectionService>();
+builder.Services.AddScoped<IResourceService, ResourceService>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IStoreRepository, StoreRepository>();
+builder.Services.AddScoped<ISectionRepository, SectionRepository>();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
 
 builder.Configuration
     .AddUserSecrets<Program>(true)
