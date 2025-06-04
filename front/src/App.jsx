25412dc180521c502fe83d481065aab98980ed1e
LacyAttorney599 <clem.blaizel@outlook.fr>
2025-06-04 15:51:55 +0200

    feat: create home route (#15)

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home'
 
-function App() {
+export default function App() {
+  return(
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
   
-
-
 }
 
-export default App
+
---
+++ front/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Rayon from "../../components/Rayon"
+
+export default function Home() {
+    return(
+        <div>
+            <Rayon/>
+        </div>
+        
+    );
+}
