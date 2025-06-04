c1988dabd8f93671b836b2e4de7c41c2ad65b471
LacyAttorney599 <clem.blaizel@outlook.fr>
2025-06-04 15:51:23 +0200

    feat: create rayon component (#15)

---
+++ front/src/components/Rayon/index.jsx
@@ -0,0 +1,12 @@
+import "../../style/Rayon/style.css"
+import rayonHorreur from "../../assets/images/rayon-horreur.png"
+
+export default function Rayon() {
+    return(
+        <div>
+            <a href=""><img src={rayonHorreur} alt="normalement c'est un rayon mon reuf" /></a>
+        </div>
+        
+    )
+    
+}
