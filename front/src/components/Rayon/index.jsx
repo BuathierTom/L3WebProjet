917f2b54e13c08d9fa0ca3d8447e16cff92d61d7
LacyAttorney599 <clem.blaizel@outlook.fr>
2025-06-04 16:33:13 +0200

    feat: show type rayon (#15)

--- front/src/components/Rayon/index.jsx
+++ front/src/components/Rayon/index.jsx
@@ -1,12 +1,29 @@
-import "../../style/Rayon/style.css"
-import rayonHorreur from "../../assets/images/rayon-horreur.png"
+import actionImg from "../../assets/images/rayon-action.png";
+import horreurImg from "../../assets/images/rayon-horreur.png";
+import comedieImg from "../../assets/images/rayon-comedie.png";
+import scifiImg from "../../assets/images/rayon-scifi.png";
 
-export default function Rayon() {
-    return(
-        <div>
-            <a href=""><img src={rayonHorreur} alt="normalement c'est un rayon mon reuf" /></a>
-        </div>
-        
-    )
-    
-}
+import "../../style/Rayon/style.css"
+const rayonImages = {
+  action: actionImg,
+  horreur: horreurImg,
+  comedie: comedieImg,
+  scifi: scifiImg,
+};
+
+const Rayon = ({ type = "action", onClick }) => {
+  const image = rayonImages[type] || actionImg;
+  const label = type.charAt(0).toUpperCase() + type.slice(1);
+
+  return (
+    <div onClick={onClick} style={{ textAlign: "center", cursor: "pointer" }}>
+      <img
+        src={image}
+        alt={`Rayon ${label}`}
+      />
+      <p style={{ fontFamily: "monospace", marginTop: "0.5rem" }}>{label}</p>
+    </div>
+  );
+};
+
+export default Rayon;
--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
+import React from "react";
 import Rayon from "../../components/Rayon"
 
 export default function Home() {
-    return(
-        <div>
-            <Rayon/>
-        </div>
-        
-    );
-}
+    return (
+    <div style={{ display: "flex", gap: "2rem", flexWrap: "wrap" }}>
+      <Rayon type="action" onClick={() => console.log("Action cliqué")} />
+      <Rayon type="horreur" onClick={() => console.log("Horreur cliqué")} />
+      <Rayon type="comedie" onClick={() => console.log("Comédie cliqué")} />
+      <Rayon type="scifi" onClick={() => console.log("Sci-Fi cliqué")} />
+    </div>
+  );
+};
+
