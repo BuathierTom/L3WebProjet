261e1e9f6dde19f894ffbe4a4e9e4adc204af0da
LacyAttorney599 <clem.blaizel@outlook.fr>
2025-06-11 16:38:34 +0200

    feat: view nav-shop / rayon (#15)
    L'affichage de la barre d'informations du magasin s'effectue avec les données dans un json.
    On peut voir chaque rayon en fonction du type.

--- front/src/components/Magasin/index.jsx
+++ front/src/components/Magasin/index.jsx
@@ -5,15 +5,21 @@ import { fetchStore } from "../../services/services";
 const Magasin = () => {
   const [buildings, setBuildings] = useState([]);
   const [resources, setResources] = useState(null);
-  const storeId = "1234"; // à remplacer par un vrai ID (ex: auth/localStorage)
+  const storeId = 'test-store'; // à remplacer par un vrai ID (ex: auth/localStorage)
 
   useEffect(() => {
   const load = async () => {
-    const response = await fetch("../../data/store.json");
-    const data = await response.json();
-    setResources(data.resources);
-    setBuildings(data.buildings);
+    try {
+      const response = await fetch("/data/store.json");
+      const data = await response.json();
+      setResources(data.resources);
+      setBuildings(data.buildings);
+    } catch (err) {
+      console.error("Erreur lors du chargement du magasin :", err);
+    }
   };
+
+
 
   load();
 }, []);
@@ -26,7 +32,7 @@ const Magasin = () => {
         </div>
       )}
 
-      <div style={{ display: "flex", gap: "2rem", flexWrap: "wrap" }}>
+      <div >
         {buildings.map((b) => (
           <Rayon key={b.id} type={b.type} />
         ))}
--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -4,14 +4,8 @@ import Magasin from "../../components/Magasin";
 
 export default function Home() {
     return (
-      
-    <div class="listRayon">
       <Magasin/>
-      <Rayon type="action" onClick={() => console.log("Action cliqué")} />
-      <Rayon type="horreur" onClick={() => console.log("Horreur cliqué")} />
-      <Rayon type="comedie" onClick={() => console.log("Comédie cliqué")} />
-      <Rayon type="scifi" onClick={() => console.log("Sci-Fi cliqué")} />
-    </div>
+    
   );
 };
 
