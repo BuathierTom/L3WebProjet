8a6ded45dd4b723e6a67d79aab3e1c69a42c66e1
buathier.tom <buathier_tom@orange.fr>
2025-06-18 14:24:08 +0200

    test: user controller tests (#22)

---
+++ back/L3WebProjet.WebAPI.Tests/UserControllerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace L3WebProjet.WebAPI.Tests;
+
+using Xunit;
+using Moq;
+using L3WebProjet.WebAPI.Controllers;
+using L3WebProjet.Business.Interfaces;
+using L3WebProjet.Common.Request;
+using L3WebProjet.Common.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+public class UserControllerTests
+{
+    [Fact]
+    public async Task CreateWithStore_Returns_Ok_With_User()
+    {
+        // On mock pour simuler les données
+        var mockService = new Mock<IUserService>();
+        var request = new UserWithStoreCreateRequest
+        {
+            Pseudo = "Tom",
+            StoreName = "VidéoTop"
+        };
+
+        var expectedUser = new UserDto
+        {
+            Id = Guid.NewGuid(),
+            Pseudo = "Tom"
+        };
+
+        mockService.Setup(s => s.CreateUserWithStoreAsync(request, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedUser);
+
+        var controller = new UserController(mockService.Object);
+
+        var result = await controller.CreateWithStore(request, CancellationToken.None);
+
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+        var user = Assert.IsType<UserDto>(createdResult.Value);
+        Assert.Equal("Tom", user.Pseudo);
+    }
+}
