c3a5d5a600c73dc3afe3d3168381db806edd318e
buathier.tom <buathier_tom@orange.fr>
2025-06-18 14:24:41 +0200

    test: resources controller tests (#22)

---
+++ back/L3WebProjet.WebAPI.Tests/ResourceControllerTests.cs
@@ -0,0 +1,51 @@
+namespace L3WebProjet.WebAPI.Tests;
+
+using Xunit;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using L3WebProjet.WebAPI.Controllers;
+using L3WebProjet.Business.Interfaces;
+
+public class ResourceControllerTests
+{
+    [Fact]
+    public async Task Calculate_ReturnsOk_WithTotalMoneyGenerated()
+    {
+        var mockService = new Mock<IResourceService>();
+        var storeId = Guid.NewGuid();
+
+        mockService.Setup(s => s.CalculateMoneyAsync(storeId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(42);
+
+        var controller = new ResourceController(mockService.Object);
+        var result = await controller.Calculate(storeId, CancellationToken.None);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+
+        var resultObject = okResult.Value;
+
+        var totalProperty = resultObject.GetType().GetProperty("total");
+        var totalValue = (int)totalProperty.GetValue(resultObject);
+
+        Assert.Equal(42, totalValue);
+    }
+
+
+    [Fact]
+    public async Task Calculate_ReturnsBadRequest_WhenServiceReturnsNegative()
+    {
+        var mockService = new Mock<IResourceService>();
+        var storeId = Guid.NewGuid();
+
+        mockService.Setup(s => s.CalculateMoneyAsync(storeId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(-1);
+
+        var controller = new ResourceController(mockService.Object);
+        var result = await controller.Calculate(storeId, CancellationToken.None);
+        var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal("Store or resource not found", badRequest.Value);
+    }
+
+}
