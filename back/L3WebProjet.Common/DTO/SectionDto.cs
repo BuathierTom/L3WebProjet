9c3d82f1a50c0f3c545bbacf0d4f5769f77bac2b
buathier.tom <buathier_tom@orange.fr>
2025-06-19 16:21:31 +0200

    imp: upgradePrice in section and balance price per level of section

--- back/L3WebProjet.Common/DTO/SectionDto.cs
+++ back/L3WebProjet.Common/DTO/SectionDto.cs
@@ -5,6 +5,7 @@ namespace L3WebProjet.Common.DTO
         public Guid Id { get; set; }
         public string Type { get; set; } = string.Empty; // Ex: "Action", "Comédie"
         public int Level { get; set; }
+        public int UpgradePrice => (int)(100 * Math.Pow(Level, 1.6));
         public bool IsUnderConstruction { get; set; }
         public DateTime? ConstructionEnd { get; set; }
         public Guid StoreId { get; set; }
